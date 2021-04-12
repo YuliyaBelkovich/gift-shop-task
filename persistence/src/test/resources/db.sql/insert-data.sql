88ad195c8ab5e7c14899f988c04dd28d1af4fb03
YuliyaBelkovich <lily.belkovich@gmail.com>
2021-04-12 15:13:50 +0300

    tests fixed

--- persistence/src/test/resources/db.sql/insert-data.sql
+++ persistence/src/test/resources/db.sql/insert-data.sql
@@ -1,6 +1,8 @@
 INSERT INTO gift_certificate(id,name,description,price,duration) VALUES (1,'test 1', 'description 1', 1.0, 1);
 INSERT INTO gift_certificate (id,name,description,price,duration) VALUES (2,'test 2', 'description 2', 2.0, 2);
 INSERT INTO gift_certificate (id,name,description,price,duration)VALUES (3,'test 3', 'description 3', 3.0, 3);
+INSERT INTO gift_certificate (id,name,description,price,duration)VALUES (4,'test 4', 'description 4', 4.0, 4);
+INSERT INTO gift_certificate (id,name,description,price,duration)VALUES (5,'test 5', 'description 5', 5.0, 5);
 INSERT INTO tag VALUES (1,'test tag 1');
 INSERT INTO tag VALUES (2,'test tag 2');
 INSERT INTO tag VALUES (3,'test tag 3');

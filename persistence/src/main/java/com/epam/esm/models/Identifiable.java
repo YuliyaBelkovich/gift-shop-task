6e1d68575b0da27671dee3683e0b12f69d4b5920
YuliyaBelkovich <lily.belkovich@gmail.com>
2021-04-15 10:06:11 +0300

    Revert "init commit"
    This reverts commit ddefac93

--- persistence/src/main/java/com/epam/esm/models/Identifiable.java
+++
@@ -1,8 +0,0 @@
-package com.epam.esm.models;
-
-public interface Identifiable {
-    int getId();
-    void setId(int id);
-
-    String getName();
-}
--- service/src/main/java/com/epam/esm/service/IdentifiableService.java
+++
@@ -1,8 +0,0 @@
-package com.epam.esm.service;
-
-import com.epam.esm.dto.ResponseGiftCertificate;
-
-
-public interface IdentifiableService {
-    public ResponseGiftCertificate findById(int id);
-}
